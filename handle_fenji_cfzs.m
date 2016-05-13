2f2755ea6c5f0b283a0c99156f4396cd9b08225c
yichengh <395076316@qq.com>
2016-05-13 18:46:42 +0800

    Fullfill handle_fenji_cfzs.

--- handle_fenji_cfzs.m
+++ handle_fenji_cfzs.m
@@ -1,4 +1,4 @@
-% 处理分级基金的拆分与折算
+% 处理分级基金的拆分与折算（折算当天停牌）
 % TODO: A:B不是5:5是否需要特殊处理
 function [cash, holding_list, share_list] = handle_fenji_cfzs(w, date, cash, holding_list, share_list)
     n = length(holding_list);
@@ -9,10 +9,21 @@ function [cash, holding_list, share_list] = handle_fenji_cfzs(w, date, cash, hol
             continue;
         end
         %fprintf('%s is fenji\n', char(holding_list(i)));
-        %折算日当天停牌
+        %获取分级基金信息
         [code_mother, code_class_a, code_class_b, type] = get_fenji_details(w, date, holding_list(i));
-        para = strcat('windcode=', holding_list(i));
-        para = strcat(para, ';startdate=', date, ';enddate=', date);
-        [w_wset_data]=w.wset('fundspitandconvert',para);
+        %获取折算信息
+        [data0]=w.wset('fundspitandconvert',strcat('windcode=', code_mother, ';startdate=', date, ';enddate=', date))
+        [data1]=w.wset('fundspitandconvert',strcat('windcode=', code_class_a, ';startdate=', date, ';enddate=', date));
+        [data2]=w.wset('fundspitandconvert',strcat('windcode=', code_class_b, ';startdate=', date, ';enddate=', date));
+        whos
+        if ~iscell(data0)
+            fprintf('不在折算日\n');
+            continue;
+        end
+        if ~iscell(data2)
+            fprintf('定折\n');
+        else
+            fprintf('非定折\n');
+        end
     end
 end
